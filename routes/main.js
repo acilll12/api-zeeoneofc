dfafda54ea70fe2565790e984e17ba4f9ba9a873
acilll12 <87960815+acilll12@users.noreply.github.com>
2021-07-26 21:18:48 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -16,9 +16,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'PatrickBot',
+            namaowner: 'Acill',
+            instagram: 'fvck_pencitraan01',
             youtube : 'Gak Punya'
         }
     }
